fae7f21f2e60ebca1d93738d8dcc4e0b4726ad2f
shathrenth <marta.nc13@gmail.com>
2022-08-28 22:37:40 +0200

    language changed to english

--- menu.py
+++ menu.py
@@ -2,13 +2,12 @@ import PySimpleGUI as sg
 sg.theme('LightGreen')
 
 # adding this comment to see if VCS works
-menu_def = ['settings', ['add an opening', ['graphics', ['option 1', 'option 2', 'option 3']]]]
 
 menu_def = [['settings', ['graphics', ['option 1', 'option 2', ], 'add an opening'], ],]
            
 layout = [[sg.Menu(menu_def)],
 		[sg.VPush()],
-		[sg.Push(), sg.Text('tu powinna byc grafika z tytulem'), sg.Push()],
+		[sg.Push(), sg.Text('title'), sg.Push()],
 		[sg.Push(), sg.Button('practice random opening', key='-random-'), sg.Push()],
 		[sg.Push(), sg.Button('choose opening to practice', key='-choose-'), sg.Push()],
 		#[sg.Push(), sg.Button('settings', key = '-settings-'), sg.Push()],
@@ -22,7 +21,7 @@ layout = [[sg.Menu(menu_def)],
 			#[sg.Push(), sg.Button('back to menu', key = '-back-'), sg.Push()],
 			#[sg.VPush()]]
 
-window = sg.Window('super szachy!', layout, size = (500, 500))
+window = sg.Window('awesome chess!', layout, size = (500, 500))
 
 #def settings():
 	#window2 = sg.Window('super szachy!: settings', layout_settings, size = (500, 500))
