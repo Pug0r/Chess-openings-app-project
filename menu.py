33c18881a764caa51e72d62d67a3016c165dea47
shathrenth <105392023+shathrenth@users.noreply.github.com>
2022-08-28 19:16:06 +0200

    menu startowe
    (bardzo prototypowy prototyp)

---
+++ menu.py
@@ -0,0 +1,45 @@
+import PySimpleGUI as sg
+sg.theme('LightGreen')
+
+menu_def = ['settings', ['add an opening', ['graphics', ['option 1', 'option 2', 'option 3']]]]
+
+menu_def = [['settings', ['graphics', ['option 1', 'option 2', ], 'add an opening'], ],]
+           
+layout = [[sg.Menu(menu_def)],
+		[sg.VPush()],
+		[sg.Push(), sg.Text('tu powinna byc grafika z tytulem'), sg.Push()],
+		[sg.Push(), sg.Button('practice random opening', key='-random-'), sg.Push()],
+		[sg.Push(), sg.Button('choose opening to practice', key='-choose-'), sg.Push()],
+		#[sg.Push(), sg.Button('settings', key = '-settings-'), sg.Push()],
+		[sg.VPush()]]
+
+
+
+#layout_settings = [[sg.VPush()],
+			#[sg.Push(), sg.Button('add an opening', key = '-add-'), sg.Push()],
+			#[sg.Push(), sg.Button('change graphics', key = '-graphics-'), sg.Push()],
+			#[sg.Push(), sg.Button('back to menu', key = '-back-'), sg.Push()],
+			#[sg.VPush()]]
+
+window = sg.Window('super szachy!', layout, size = (500, 500))
+
+#def settings():
+	#window2 = sg.Window('super szachy!: settings', layout_settings, size = (500, 500))
+	#event, values = window2.read()
+	#if event=='-back-':
+		#window2.close()
+		#window.reappear()
+
+
+while True:
+	event, values = window.read()
+	if event in (sg.WIN_CLOSED, 'Cancel'):
+		break
+	elif event == '-random-':
+		pass
+	elif event == '-choose-':
+		pass
+	#elif event == '-settings-':
+		#pass
+	
+window.close()
